664a061aca3a8cef15e1cac4255ffabdf8f75ba3
tbrams <torben@brams.dk>
2017-01-14 00:31:18 +0700

    commit on keyboard enter now

--- app/src/main/java/dk/brams/android/guessqubicfactor/MainActivity.java
+++ app/src/main/java/dk/brams/android/guessqubicfactor/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -19,51 +20,78 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "TBR:";
-    private static int mSecret = 0;
-    private static int mGood=0;
-    private static int mBad=0;
-    private static boolean mNewHighScoreAnnounced=false;
     private Activity mActivity=this;
-    private static SharedPreferences mSharedPrefs;
+
+    private static int sSecret = 0;
+    private static int sGood =0;
+    private static int sBad =0;
+    private static SharedPreferences sSharedPreferences;
+    private static EditText sAnswerField;
+    private static ImageView sImgView;
+    private static MyRunnable sRunnable;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 
-        final EditText answerField = (EditText) findViewById(R.id.answer_text);
-        final MyRunnable mRunnable = new MyRunnable(this);
-        final ImageView imgView = (ImageView) findViewById(R.id.imageGrade);
+        sSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sRunnable = new MyRunnable(this);
+        sImgView = (ImageView) findViewById(R.id.imageGrade);
+
+        sAnswerField = (EditText) findViewById(R.id.answer_text);
+        sAnswerField.setOnKeyListener(new View.OnKeyListener()
+        {
+            public boolean onKey(View v, int keyCode, KeyEvent event)
+            {
+                if (event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    switch (keyCode)
+                    {
+                        case KeyEvent.KEYCODE_DPAD_CENTER:
+                        case KeyEvent.KEYCODE_ENTER:
+                            processAnswer(v);
+                            return true;
+                        default:
+                            break;
+                    }
+                }
+                return false;
+            }
+        });
 
         // Create a new challenge and fill in fields
         newQuestion(this);
 
-        imgView.setOnClickListener(new View.OnClickListener() {
+        sImgView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String answer=answerField.getText().toString();
-                if (!answer.equals("")) {
-                    if (Integer.parseInt(answer) == mSecret) {
-                        imgView.setBackgroundResource(R.mipmap.ic_thumbs_up);
-                        mGood++;
-                    } else {
-                        imgView.setBackgroundResource(R.mipmap.ic_thumbs_down);
-
-                        Snackbar.make(view, getString(R.string.correct_answer)+mSecret, Snackbar.LENGTH_LONG).show();
-                        mBad++;
-                    }
-
-                    updateScore(mActivity);
-                }
-
-                // Execute the Runnable in 3 seconds
-                mHandler.postDelayed(mRunnable, 3000);
-
+                processAnswer(view);
             }
         });
 
+    }
+
+
+    void processAnswer(View view) {
+        String answer= sAnswerField.getText().toString();
+        if (!answer.equals("")) {
+            if (Integer.parseInt(answer) == sSecret) {
+                sImgView.setBackgroundResource(R.mipmap.ic_thumbs_up);
+                sGood++;
+            } else {
+                sImgView.setBackgroundResource(R.mipmap.ic_thumbs_down);
+
+                Snackbar.make(view, getString(R.string.correct_answer)+ sSecret, Snackbar.LENGTH_LONG).show();
+                sBad++;
+            }
+
+            updateScore(mActivity);
+        }
+
+        // Execute the Runnable in 3 seconds
+        mHandler.postDelayed(sRunnable, 3000);
     }
 
 
@@ -92,17 +120,17 @@ public class MainActivity extends AppCompatActivity {
 
     private static void newQuestion(Activity activity){
 
-        // set the question icon back
+        // set the answer icon back
         ImageView img = (ImageView) activity.findViewById(R.id.imageGrade);
         img.setBackgroundResource(R.mipmap.ic_launcher);
 
         // Find a new secret number
         Random r = new Random();
-        mSecret = r.nextInt(100-11)+11;
+        sSecret = r.nextInt(100-11)+11;
 
         // set the big number
         TextView number = (TextView) activity.findViewById(R.id.big_number_text);
-        number.setText(Integer.toString(mSecret*mSecret*mSecret));
+        number.setText(Integer.toString(sSecret * sSecret * sSecret));
 
         // clear the previous answer
         EditText ans = (EditText) activity.findViewById(R.id.answer_text);
@@ -114,18 +142,18 @@ public class MainActivity extends AppCompatActivity {
 
     private static void updateScore(Activity activity) {
         TextView scoreField = (TextView) activity.findViewById(R.id.text_score);
-        int score = mGood*10-mBad*20;
+        int score = sGood *10- sBad *20;
         scoreField.setText(String.format("%d", score));
 
-        int prevRecord = mSharedPrefs.getInt(activity.getString(R.string.highScore), 0);
+        int prevRecord = sSharedPreferences.getInt(activity.getString(R.string.highScore), 0);
         if (score>prevRecord) {
             // Make quick announcement
             Toast.makeText(activity, R.string.highScoreMsg, Toast.LENGTH_SHORT).show();
 
             // Write to preferences
-            SharedPreferences.Editor editor = mSharedPrefs.edit();
+            SharedPreferences.Editor editor = sSharedPreferences.edit();
             editor.putInt(activity.getString(R.string.highScore), score);
-            editor.commit();
+            editor.apply();
         }
 
 
